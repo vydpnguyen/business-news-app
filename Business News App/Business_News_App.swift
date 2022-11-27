1b685cae9fa3e86bc70aab00fd969ce860e5e4b3
Vy Nguyen <vydpnguyen@gmail.com>
2022-11-27 14:27:04 -0800

    Initial Commit

---
+++ Business News App/Business_News_App.swift
@@ -0,0 +1,17 @@
+//
+//  Business_News_AppApp.swift
+//  Business News App
+//
+//  Created by Vy Dang Phuong Nguyen on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Business_News_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Business News App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Business News App
+//
+//  Created by Vy Dang Phuong Nguyen on 11/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
